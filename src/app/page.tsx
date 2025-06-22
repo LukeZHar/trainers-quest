da902ce0e4e979576732a92d79be2e3884f0c776
LukeZHar <luke.z.harris@hotmail.com>
2025-06-22 22:36:12 +1000

    feat: change in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-
+      <h1 className="text-4xl font-bold">Welcome to Thirdweb with Next.js!</h1>
       <p className="mt-4 text-lg">
-        This is a simple Next.js application.
+        This is a starter template for building applications with Thirdweb and Next.js.
       </p>
     </main>
   );
