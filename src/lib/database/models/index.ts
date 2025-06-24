9222611364b76a80edb1de6df85f8a08b7c413db
LukeZHar <luke.z.harris@hotmail.com>
2025-06-24 19:26:01 +1000

    FeaT: Card class for mongo

--- src/lib/database/models/index.ts
+++ src/lib/database/models/index.ts
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 import { type ReturnModelType, getModelForClass } from "@typegoose/typegoose";
 import { User } from "../classes/User";
+import { Card } from "../classes/Card";
 
 export const UserModel =
   (mongoose.models.User as ReturnModelType<typeof User> | undefined) ??
   getModelForClass(User);
+
+export const CardModel =
+  (mongoose.models.Card as ReturnModelType<typeof Card> | undefined) ??
+  getModelForClass(Card);
